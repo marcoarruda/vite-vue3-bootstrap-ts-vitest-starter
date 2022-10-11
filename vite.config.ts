eecf97e7bf80852f87e28eca57180391208962b5
Marco Antonio Arruda <marco.nc.arruda@gmail.com>
2022-10-11 14:45:22 -0300

    install vitest w/ plugins and configure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import type { UserConfig as VitestUserConfigInterface } from "vitest/config";
+
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+};
+
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  test: vitestConfig.test,
+});
